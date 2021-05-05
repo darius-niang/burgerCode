f99c263daa100878683fe717ec3af88905bc5ae2
darius-niang <64025953+darius-niang@users.noreply.github.com>
2021-05-05 16:04:38 +0000

    Add files via upload

---
+++ database.php
@@ -0,0 +1,27 @@
+<?php
+
+class Database{
+    private static $dbHost = "localhost";
+    private static $dbName = "burger_code";
+    private static $dbUser = "root";
+    private static $dbUserPassword = "";
+
+    private static $connection = null;
+
+    public static function connect(){
+        try{
+            self::$connection = new PDO("mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName,self::$dbUser,self::$dbUserPassword);
+        }
+        catch(PDOException $a){
+
+            die($a->getMessage());
+        }
+        return self::$connection;
+    }
+    public static function disconnect(){
+        self::$connection = null;
+    }
+}
+
+
+?>
